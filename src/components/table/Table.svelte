f7a8260bcf05eb9d084de8db74b2ce801f01b333
Pedro De León Bahena <pedrodeleonbahena26@gmail.com>
2024-12-19 19:13:50 -0600

    update: Table desing

---
+++ src/components/table/Table.svelte
@@ -0,0 +1,43 @@
+<script>
+    import "./Table.css";
+    import menuIcon from ".././../../src/images/menuIcon.svg";
+</script>
+
+<table class="shadow-z">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Juan Pérez</td>
+            <td>30</td>
+            <td>
+                <div class="icon">
+                    <img src={menuIcon} alt="Menu Icon" />
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>María López</td>
+            <td>25</td>
+            <td>
+                <div class="icon">
+                    <img src={menuIcon} alt="Menu Icon" />
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Carlos García</td>
+            <td>35</td>
+            <td>
+                <div class="icon">
+                    <img src={menuIcon} alt="Menu Icon" />
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
