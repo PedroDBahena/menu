e4f90ab7712c0346af889f516abfe9ee209bf020
Pedro De León Bahena <pedrodeleonbahena26@gmail.com>
2025-01-07 13:26:14 -0600

    update: Calendar

--- src/components/date-picker/calendar/Calendar.svelte
+++ src/components/date-picker/calendar/Calendar.svelte
@@ -1,171 +1,202 @@
 <script>
-    import "./Calendar.css";
-    import leftSvgrepo from "../../../images/leftSvgrepo.svg";
-    import leftArrow from "../../../images/leftArrow.svg";
-    import rightArrow from "../../../images/rightArrow.svg";
-    import rightSvgrepo from "../../../images/rightSvgrepo.svg";
-    import { onMount } from "svelte";
+  import "./Calendar.css";
+  import leftSvgrepo from "../../../images/leftSvgrepo.svg";
+  import leftArrow from "../../../images/leftArrow.svg";
+  import rightArrow from "../../../images/rightArrow.svg";
+  import rightSvgrepo from "../../../images/rightSvgrepo.svg";
+  import { onMount, tick } from "svelte";
+  import { json } from "@sveltejs/kit";
 
-    let currentMonth = new Date().getMonth();
-    let currentYear = new Date().getFullYear();
-    let currentDays = 0;
-    let daysOfCalendar = Array(7)
-        .fill(null)
-        .map(() => []);
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let currentDays = 0;
+  let daysByWeek = [[], [], [], [], [], []];
 
-    export let months;
-    export let daysOfWeek;
+  export let months;
+  export let daysOfWeek;
 
-    function increaseMonth() {
-        currentMonth++;
+  function increaseMonth() {
+    currentMonth++;
 
-        if (currentMonth > 11) {
-            currentMonth = 0;
-            increaseYear();
-        }
-
-        getTotalDays();
-        fillCalendar();
+    if (currentMonth > 11) {
+      currentMonth = 0;
+      increaseYear();
     }
 
-    function decreaseMonth() {
-        currentMonth--;
+    generateCalendar();
+  }
 
-        if (currentMonth < 0) {
-            currentMonth = 11;
-            decreaseYear();
-        }
+  function decreaseMonth() {
+    currentMonth--;
 
-        getTotalDays();
-        fillCalendar();
+    if (currentMonth < 0) {
+      currentMonth = 11;
+      decreaseYear();
     }
 
-    function increaseYear() {
-        currentYear++;
-        getTotalDays();
-        fillCalendar();
+    generateCalendar();
+  }
+
+  function increaseYear() {
+    currentYear++;
+    generateCalendar();
+  }
+
+  function decreaseYear() {
+    currentYear--;
+    generateCalendar();
+  }
+
+  function getTotalDays() {
+    currentDays = new Date(currentYear, currentMonth + 1, 0).getDate();
+  }
+
+  function generateCalendar() {
+    getTotalDays();
+    daysByWeek = fillWeeks();
+    console.warn(daysByWeek);
+  }
+
+  function getCurrentMonthRange() {
+    const firstDay = new Date(currentYear, currentMonth, 1);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+
+    return {
+      firstDay: firstDay?.getDay(),
+      lastDay: lastDay?.getDay(),
+    };
+  }
+
+  function generatePreviousOrNextDays(count, getPrevMonthDays) {
+    const adjustedDay = getPrevMonthDays ? 0 : 1;
+    let month = getPrevMonthDays ? currentMonth - 1 : currentMonth;
+    month = month < 0 ? 12 : month;
+    let year = month == 12 ? currentYear - 1 : currentYear;
+
+    const initialDay = new Date(year, month, adjustedDay);
+
+    const days = [];
+
+    if (getPrevMonthDays) {
+      for (let i = count - 1; i >= 0; i--) {
+        const day = new Date(
+          year,
+          month < 0 ? 11 : month,
+          initialDay?.getDate() - i
+        );
+        days?.push(day?.getDate());
+      }
+    } else {
+      for (let i = 0; i < count; i++) {
+        const day = new Date(
+          year,
+          month < 0 ? 11 : month,
+          initialDay?.getDate() + i
+        );
+        days?.push(day?.getDate());
+      }
     }
 
-    function decreaseYear() {
-        currentYear--;
-        getTotalDays();
-        fillCalendar();
+    return days;
+  }
+
+  function fillWeeks() {
+    let days = [[], [], [], [], [], []];
+    const monthRange = getCurrentMonthRange();
+
+    // DOCS: Obtiene los últimos días del mes y los agrega a la primer semana del mes actual
+    days[0].push(
+      ...generatePreviousOrNextDays(monthRange.firstDay, true)
+    );
+
+    const firstDays = days[0]?.length;
+
+    let countWeeks = 1;
+
+    for (let i = 1; i <= currentDays; i++) {
+      if (i <= 7 - firstDays) {
+        // DOCS: Guarda los días de la primer semana, incluyendo los últimos días del mes anterior
+        days[0]?.push(i);
+      } else {
+        // DOCS: Guarda el resto de los días por semana
+        countWeeks =
+          days[countWeeks]?.length === 7 ? countWeeks + 1 : countWeeks;
+
+        days[countWeeks]?.push(i);
+      }
     }
 
-    function getTotalDays() {
-        currentDays = new Date(currentYear, currentMonth + 1, 0).getDate();
+    // DOCS: Obtiene los arreglos de las semanas incompletas
+    let incompleteWeeks = days
+      .map((subarray, index) =>
+        subarray.length !== 7 ? { index, subarray } : null
+      )
+      .filter((item) => item !== null);
+
+    // DOSC: Calcular cuántos elementos se necesitan para llenar los subarreglos
+    let neededDays = incompleteWeeks.reduce(
+      (total, week) =>
+        week?.subarray.length !== 7
+          ? total + (7 - week?.subarray.length)
+          : total,
+      0
+    );
+
+    // DOCS: Obtiene los primeros días del mes siguiente
+    const nextMonthDays = generatePreviousOrNextDays(neededDays, false);
+
+    // DOCS: Acompleta las semanas incompletas
+    for (let i = 0; i < incompleteWeeks?.length; i++) {
+      const index = incompleteWeeks[i]?.index;
+      const missingDays = 7 - days[index]?.length;
+
+      days[index].push(...nextMonthDays.slice(0, missingDays));
+      nextMonthDays?.splice(0, missingDays);
     }
 
-    function fillCalendar() {
-        daysOfCalendar = daysOfWeek.map((day) => [day, []]);
+    return days;
+  }
 
-        console.warn('daysOfCalendar', daysOfCalendar);
-        
-        for (let i = 1; i <= currentDays; i++) {
-            const dayOfWeek = new Date(currentYear, currentMonth, i)?.getDay();
-
-            daysOfCalendar[dayOfWeek][1]?.push(i);
-        }
-        
-        daysOfCalendar = completeDays(daysOfCalendar);
-    }
-
-    function completeDays(days) {
-        days.forEach((item, index) => {
-            const length = item[1]?.length;
-            if (length === 4) {
-                item[1]?.unshift(getDaysByWeekAndMonth(index, true));
-                item[1]?.push(getDaysByWeekAndMonth(index, false));
-            } else if (length === 5) {
-                item[1]?.push(getDaysByWeekAndMonth(index, false));
-            }
-        });
-
-        return days;
-    }
-
-    function getDaysByWeekAndMonth(dayIndex, isPreviousMonth) {
-        let month = currentMonth;
-        let year = currentYear;
-        let initialDay = 1;
-
-        if (isPreviousMonth) {
-            month = isPreviousMonth
-                ? month === 0
-                    ? 12
-                    : month - 1
-                : month + 1;
-
-            if(month === 12) {
-                year = currentYear - 1;
-                initialDay = 0;
-            }
-        } else {
-            month = month + 1;
-        }
-
-        let date = new Date(year, month, initialDay);
-
-        while (date.getDay() !== dayIndex) {
-            if(isPreviousMonth) {
-                date.setDate(date.getDate() - 1);
-            } else {
-                date.setDate(date.getDate() + 1);
-            }
-            
-        }
-
-        while (date.getMonth() === currentMonth) {
-            if(isPreviousMonth) {
-                date.setDate(date.getDate() - 7);
-            } else {
-                date.setDate(date.getDate() + 7);
-            }
-        }
-
-        return date?.getDate();
-    }
-
-    onMount(() => {
-        getTotalDays();
-        fillCalendar();
-    });
+  onMount(async () => {
+    getTotalDays();
+    generateCalendar();
+  });
 </script>
 
 <div id="calendar">
-    <div class="header">
-        <button on:click={decreaseYear}>
-            <img src={leftSvgrepo} alt="" />
-        </button>
+  <div class="header">
+    <button on:click={decreaseYear}>
+      <img src={leftSvgrepo} alt="" />
+    </button>
 
-        <button on:click={decreaseMonth}>
-            <img src={leftArrow} alt="" />
-        </button>
+    <button on:click={decreaseMonth}>
+      <img src={leftArrow} alt="" />
+    </button>
 
-        <p>{months[currentMonth]}</p>
-        <p>{currentYear}</p>
+    <p>{months[currentMonth]}</p>
+    <p>{currentYear}</p>
 
-        <button on:click={increaseMonth}>
-            <img src={rightArrow} alt="" />
-        </button>
-        <button on:click={increaseYear}>
-            <img src={rightSvgrepo} alt="" />
-        </button>
-    </div>
+    <button on:click={increaseMonth}>
+      <img src={rightArrow} alt="" />
+    </button>
+    <button on:click={increaseYear}>
+      <img src={rightSvgrepo} alt="" />
+    </button>
+  </div>
 
-    <div class="week">
-        {#each daysOfWeek as day}
-            <p>{day}</p>
+  <div class="week">
+    {#each daysOfWeek as day}
+      <p>{day}</p>
+    {/each}
+  </div>
+
+  <div class="days-of-week">
+    {#each daysByWeek as week}
+      <div class="week">
+        {#each week as day}
+          <span>{day}</span>
         {/each}
-    </div>
-
-    <div class="days-of-week">
-        {#each daysOfCalendar as [day, numbers], index}
-            <div class="day-container">
-                {#each numbers as number}
-                    <span>{number}</span>
-                {/each}
-            </div>
-        {/each}
-    </div>
+      </div>
+    {/each}
+  </div>
 </div>
