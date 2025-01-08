56ba11667c6e6f24e394b30b2a7bdd97899260c7
Pedro De León Bahena <pedrodeleonbahena26@gmail.com>
2025-01-07 21:36:36 -0600

    test: github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,9 +9,9 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
-    paths: {
-      base: "/menu",
-    },
+	paths: {
+		base: dev ? '' : '/menu',
+	}
   },
 };
 
