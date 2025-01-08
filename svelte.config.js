169ba36b616852913cb4cf3e6d5394d7887dabc5
Pedro De León Bahena <pedrodeleonbahena26@gmail.com>
2025-01-07 20:24:12 -0600

    test: github pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			bnase: process.env.NODE_ENV === 'production' ? '/menu' : '',
+			base: process.env.NODE_ENV === 'production' ? '/menu' : '',
 		}
 	}
 };
