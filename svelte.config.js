f3c44e74405ed1f40f98bcf9a9106769bf475129
Pedro De León Bahena <pedrodeleonbahena26@gmail.com>
2025-01-07 20:28:49 -0600

    test: github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocess } from 'svelte/compiler';
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = process.argv.includes('dev');
+
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/menu' : '',
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
